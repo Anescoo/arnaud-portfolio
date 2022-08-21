e801c87b402d9e452214ea49c328ed14cfe18db4
Anescoo <72858178+Anescoo@users.noreply.github.com>
2022-08-21 17:43:25 +0200

    add server express node

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const path = require('path');
-const jwt = require('jsonwebtoken');
 const express = require('express');
+const port = 3000;
 
 // Instanciation du serveur
 var server = express();
 
 // Routes
-server.get('/', function (req, res){
-	res.setHeader('Content-Type','text/html');
-	res.sendFile(path.join(__dirname ,'./frontend/templates/index.html'));
+server.get('/', (req, res) => {
+	res.setHeader('Content-Type','text/html')
+	res.sendFile(path.join(__dirname ,'index.html'))
 	server.use(express.static('css'));
-	server.use('csss', express.static(__dirname + 'css'));
-});
+	server.use('/css', express.static(__dirname + '/css'));
+})
 
-console.log('Le serveur fonctionne sur le port : 3000. Pour ce serveur, vous pouvez accéder à : http://127.0.0.1:3000');
+server.listen(port, ()=> {
+	console.log('Le serveur fonctionne sur le port : 3000. Pour ce serveur, vous pouvez accéder à : http://127.0.0.1:3000');
+})
 
-server.use(express.static('css')); //tell the server that ./public/ contains the static webpages
-var io=require('socket.io')(port);
+
+
+// server.use(express.static('css')); //tell the server that ./public/ contains the static webpages
