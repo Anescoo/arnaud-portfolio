44637b68eddfb07fe89f7fd984c73317de2b40bd
Anescoo <72858178+Anescoo@users.noreply.github.com>
2022-08-21 21:17:01 +0200

    maj

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ server.get('/', (req, res) => {
 
 server.use(express.static('css','fonts','images','inc','js'))
 server.use('/css', express.static(__dirname + '/css'))
-server.use('/fonts', express.static(__dirname + 'fonts'))
-server.use('/images', express.static(__dirname + 'images'))
-server.use('/inc', express.static(__dirname + 'inc'))
-server.use('/js', express.static(__dirname + 'js'))
+
 
 server.listen(port, ()=> {
 	console.log('Le serveur fonctionne sur le port : 3000. Pour ce serveur, vous pouvez accéder à : http://127.0.0.1:3000');
